b92eee1bd2a970e9f0eefc4c19aefeaed84ef562
Deepakdhore <87926172+Deepakdhore@users.noreply.github.com>
2024-11-28 11:23:28 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("this is java program ");
+        System.out.println("output from java programe");
+        System.out.println("hello");
+        System.out.println(new Date());
+    }
+}
